6716a139bc33a2cc6e04a55cff2fb88492a9169a
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-11-16 15:24:37 +0000

    stable

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://4eb73926bb40.ngrok.io"
+  baseURL: "http://ee7b57064e53.ngrok.io"
 });
 
 
-// in track server, type ngrock http 3000 replace the above baseURL with the one generated.
+// in track server, type ngrok http 3000 replace the above baseURL with the one generated.
 // "http://[12charCode].ngrok.io"
